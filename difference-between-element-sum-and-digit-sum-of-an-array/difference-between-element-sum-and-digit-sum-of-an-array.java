0508b700bbad5c028a542853ddd9771c056f03f7
rana782782 <janardanbaitha3@gmail.com>
2023-06-03 17:54:23 +0000

    LeetCode: Difference Between Element Sum and Digit Sum of an Array (difference-between-element-sum-and-digit-sum-of-an-array) — placeholder (solved on 2023-06-03T17:54:23Z)

---
+++ difference-between-element-sum-and-digit-sum-of-an-array/difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -0,0 +1,6 @@
+# Placeholder for Difference Between Element Sum and Digit Sum of an Array (difference-between-element-sum-and-digit-sum-of-an-array)
+
+- **Submission ID:** 963130000
+- **Original solve time (UTC):** 2023-06-03T17:54:23Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
