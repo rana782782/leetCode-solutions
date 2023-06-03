10cedbfd2fccb5d4b7852dc53b8173d4f4f9948b
rana782782 <janardanbaitha3@gmail.com>
2023-06-03 09:50:30 +0000

    LeetCode: Sign of the Product of an Array (sign-of-the-product-of-an-array) — placeholder (solved on 2023-06-03T09:50:30Z)

---
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -0,0 +1,6 @@
+# Placeholder for Sign of the Product of an Array (sign-of-the-product-of-an-array)
+
+- **Submission ID:** 962884540
+- **Original solve time (UTC):** 2023-06-03T09:50:30Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
