7b1d5e679230dcd260a0bac943cd3bf013eb0ba7
rana782782 <janardanbaitha3@gmail.com>
2023-06-04 10:21:16 +0000

    LeetCode: Count Equal and Divisible Pairs in an Array (count-equal-and-divisible-pairs-in-an-array) — placeholder (solved on 2023-06-04T10:21:16Z)

---
+++ count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.java
@@ -0,0 +1,6 @@
+# Placeholder for Count Equal and Divisible Pairs in an Array (count-equal-and-divisible-pairs-in-an-array)
+
+- **Submission ID:** 963608065
+- **Original solve time (UTC):** 2023-06-04T10:21:16Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
