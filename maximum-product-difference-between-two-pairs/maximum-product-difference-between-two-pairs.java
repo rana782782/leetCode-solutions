f478008e631c42a7ffbf4e44c4a649fb7e365708
rana782782 <janardanbaitha3@gmail.com>
2023-06-04 10:10:38 +0000

    LeetCode: Maximum Product Difference Between Two Pairs (maximum-product-difference-between-two-pairs) — placeholder (solved on 2023-06-04T10:10:38Z)

---
+++ maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -0,0 +1,6 @@
+# Placeholder for Maximum Product Difference Between Two Pairs (maximum-product-difference-between-two-pairs)
+
+- **Submission ID:** 963602916
+- **Original solve time (UTC):** 2023-06-04T10:10:38Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
