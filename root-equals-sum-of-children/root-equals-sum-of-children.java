505e866b798a2f42362b4cec4372f7a3c0802608
rana782782 <janardanbaitha3@gmail.com>
2023-07-17 14:16:45 +0000

    LeetCode: Root Equals Sum of Children (root-equals-sum-of-children) — placeholder (solved on 2023-07-17T14:16:45Z)

---
+++ root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,6 @@
+# Placeholder for Root Equals Sum of Children (root-equals-sum-of-children)
+
+- **Submission ID:** 996741137
+- **Original solve time (UTC):** 2023-07-17T14:16:45Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
