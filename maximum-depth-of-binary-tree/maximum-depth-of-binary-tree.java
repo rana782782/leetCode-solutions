8a5aeff4bafd43bb0fe6b9370e6b65b62e970bec
rana782782 <janardanbaitha3@gmail.com>
2023-07-17 14:45:23 +0000

    LeetCode: Maximum Depth of Binary Tree (maximum-depth-of-binary-tree) — placeholder (solved on 2023-07-17T14:45:23Z)

---
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,6 @@
+# Placeholder for Maximum Depth of Binary Tree (maximum-depth-of-binary-tree)
+
+- **Submission ID:** 996763098
+- **Original solve time (UTC):** 2023-07-17T14:45:23Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
