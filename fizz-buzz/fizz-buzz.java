004e094f80b255be33d0dc0186efd211618e16c4
rana782782 <janardanbaitha3@gmail.com>
2024-09-09 07:01:04 +0000

    LeetCode: Fizz Buzz (fizz-buzz) — solved on 2024-09-09T07:01:04Z

---
+++ fizz-buzz/fizz-buzz.java
@@ -0,0 +1,28 @@
+// LeetCode: Fizz Buzz (fizz-buzz)
+// Submission ID: 1383980286
+// Language: java
+// Timestamp (UTC): 2024-09-09T07:01:04Z
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> output = new ArrayList<>();
+ 
+
+          for(int i = 1; i<=n; i++){
+            if(i%(3*5) == 0){
+                output.add("FizzBuzz");
+            }
+            else if(i%3 == 0){
+                output.add("Fizz");
+            }
+            else if(i%5 == 0){
+                output.add("Buzz");
+            }
+            else{
+                output.add(String.valueOf(i));
+            }
+          }
+
+          return output;
+    }
+}
