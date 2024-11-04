448cb0e9e3175dbe9113f46f53bb7a49b5e3b042
rana782782 <janardanbaitha3@gmail.com>
2024-11-04 06:12:02 +0000

    LeetCode: Stone Game (stone-game) — solved on 2024-11-04T06:12:02Z

---
+++ stone-game/stone-game.java
@@ -0,0 +1,10 @@
+// LeetCode: Stone Game (stone-game)
+// Submission ID: 1442449733
+// Language: java
+// Timestamp (UTC): 2024-11-04T06:12:02Z
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
