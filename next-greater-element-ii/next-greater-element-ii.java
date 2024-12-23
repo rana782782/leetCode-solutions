f7821fbfd75830fcb2f2d183a3b4e477d9782dfe
rana782782 <janardanbaitha3@gmail.com>
2024-12-23 09:19:58 +0000

    LeetCode: Next Greater Element II (next-greater-element-ii) — placeholder (solved on 2024-12-23T09:19:58Z)

--- next-greater-element-ii/next-greater-element-ii.java
+++ next-greater-element-ii/next-greater-element-ii.java
@@ -1,42 +1,6 @@
-// LeetCode: Next Greater Element II (next-greater-element-ii)
-// Submission ID: 1486143326
-// Language: java
-// Timestamp (UTC): 2024-12-23T09:19:58Z
+# Placeholder for Next Greater Element II (next-greater-element-ii)
 
-class Solution {
-   	 public int[] nextGreaterElements(int[] nums) {
-		 if(nums.length == 1) {
-			 int ans[] = new int[1];
-			 ans[0] = -1;
-			 return ans;
-		 }
-	        Stack<Integer> st = new Stack<>();
-	        int output[] = new int[nums.length];
-	        
-	        for(int i = nums.length-2; i>=0; i--) {
-	        	while(st.size()!=0 && nums[i]>=st.peek()) {
-	        		st.pop();
-	        	}
-	        	st.push(nums[i]);
-	        }
-	        
-	        for(int i = nums.length-1; i>=0; i--) {
-	        	while(st.size()!=0 && nums[i]>=st.peek()) {
-	        		st.pop();
-	        	}
-	        	
-	        	if(st.size() == 0) {
-	        		output[i] = -1;
-	        	}
-	        	
-	        	else {
-	        		output[i] = st.peek();
-	        	}
-	        	
-	        	st.push(nums[i]);
-	        }
-	        
-	        return output;
-	        
-	    }
-}
+- **Submission ID:** 1486143326
+- **Original solve time (UTC):** 2024-12-23T09:19:58Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
