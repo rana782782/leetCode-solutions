b80958e597cfea98156f578a59a23a363e265a4a
rana782782 <janardanbaitha3@gmail.com>
2025-01-02 18:20:37 +0000

    LeetCode: Pacific Atlantic Water Flow (pacific-atlantic-water-flow) — placeholder (solved on 2025-01-02T18:20:37Z)

--- pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,57 +1,6 @@
-// LeetCode: Pacific Atlantic Water Flow (pacific-atlantic-water-flow)
-// Submission ID: 1495420148
-// Language: java
-// Timestamp (UTC): 2025-01-02T18:20:37Z
+# Placeholder for Pacific Atlantic Water Flow (pacific-atlantic-water-flow)
 
-class Solution {
-   	 public List<List<Integer>> pacificAtlantic(int[][] heights) {
-	        List<List<Integer>> output = new ArrayList<>();
-	        if(heights == null || heights.length == 0 || heights[0].length == 0) {
-	        	return output;
-	        }
-	        
-	         int row = heights.length;
-	          int col = heights[0].length;
-	          
-	          
-	          boolean[][]  pacific = new boolean[row][col];
-	          boolean[][]  atlantic = new boolean[row][col];
-	          
-	          for(int i = 0; i<row; i++) {
-	        	  solution(heights, i, 0, Integer.MIN_VALUE, pacific);
-	        	  solution(heights, i, col-1, Integer.MIN_VALUE, atlantic);
-	          }
-	          for(int i = 0; i<col; i++) {
-	        	  solution(heights, 0, i,Integer.MIN_VALUE, pacific);
-	        	  solution(heights, row-1, i, Integer.MIN_VALUE, atlantic);
-	          }
-	          
-	          
-	          
-	          
-	          
-	          
-	          for(int i = 0; i<row; i++) {
-	        	  for(int j = 0; j<col; j++) {
-	        		  if(pacific[i][j]&& atlantic[i][j]) {
-	        			  output.add(Arrays.asList(i,j));
-	        		  }
-	        	  }
-	          }
-	          
-	          return output;
-	    }
-	static int dir[][] = {{-1,0},{1,0},{0,-1},{0,1}};
-	 public static void solution(int matrix[][], int i, int j, int prev, boolean[][] ocean) {
-		 if(i<0 || i>=matrix.length || j<0 || j>=matrix[0].length || ocean[i][j] || (matrix[i][j]<prev)) return;
-		 
-		 ocean[i][j] = true;
-		 
-		 for(int[] d : dir) {
-			 solution(matrix, i+d[0], j+d[1], matrix[i][j], ocean);
-		 }
-		 
-		 
-		 
-	 }
-}
+- **Submission ID:** 1495420148
+- **Original solve time (UTC):** 2025-01-02T18:20:37Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
