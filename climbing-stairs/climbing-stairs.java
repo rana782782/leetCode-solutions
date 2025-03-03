a5821ef7c983a43b53765e2025d5c9c28c274f68
rana782782 <janardanbaitha3@gmail.com>
2025-03-03 15:00:40 +0000

    LeetCode: Climbing Stairs (climbing-stairs) — solved on 2025-03-03T15:00:40Z

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+// LeetCode: Climbing Stairs (climbing-stairs)
+// Submission ID: 1561531906
+// Language: java
+// Timestamp (UTC): 2025-03-03T15:00:40Z
+
+class Solution {
+    public int climbStairs(int n) {
+         int dp[] = new int[n+1];
+	       dp[0] = 1;
+	       dp[1] = 1;
+	       for(int i =2; i<dp.length; i++) {
+	    	   dp[i] = dp[i-1]+dp[i-2];
+	       }
+	       return dp[n];
+    }
+}
