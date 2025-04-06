5283cf73fad17b8af7432be9ad1f8ea3e26e46b6
rana782782 <janardanbaitha3@gmail.com>
2025-04-06 18:33:28 +0000

    LeetCode: Eliminate Maximum Number of Monsters (eliminate-maximum-number-of-monsters) — placeholder (solved on 2025-04-06T18:33:28Z)

--- eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
+++ eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.java
@@ -1,28 +1,6 @@
-// LeetCode: Eliminate Maximum Number of Monsters (eliminate-maximum-number-of-monsters)
-// Submission ID: 1598826415
-// Language: java
-// Timestamp (UTC): 2025-04-06T18:33:28Z
+# Placeholder for Eliminate Maximum Number of Monsters (eliminate-maximum-number-of-monsters)
 
-class Solution {
-    public int eliminateMaximum(int[] dist, int[] speed) {
-        double time[] = new double[dist.length];
-
-        for(int i = 0;i<dist.length; i++){
-            time[i] = (double)dist[i]/speed[i];
-        }
-
-        Arrays.sort(time);
-
-        int count = 0;
-
-        for(int i = 0; i<time.length; i++){
-            if(time[i]>i)count++;
-
-            else{
-                break;
-            }
-        }
-
-        return count;
-    }
-}
+- **Submission ID:** 1598826415
+- **Original solve time (UTC):** 2025-04-06T18:33:28Z
+- **Note:** LeetCode API did not return code for this submission.
+  This placeholder ensures a backdated commit for your contribution graph.
