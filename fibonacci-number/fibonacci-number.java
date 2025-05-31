a4cf298426b56370a918994f601da987e0de8600
rana782782 <janardanbaitha3@gmail.com>
2025-05-31 16:10:40 +0000

    LeetCode: Fibonacci Number (fibonacci-number) — solved on 2025-05-31T16:10:40Z

---
+++ fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+// LeetCode: Fibonacci Number (fibonacci-number)
+// Submission ID: 1649886698
+// Language: java
+// Timestamp (UTC): 2025-05-31T16:10:40Z
+
+class Solution {
+
+    public int fib(int n) {
+        if(n == 1 || n == 0)return n;
+       int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1]  = 1;
+        
+        for(int i = 2; i<dp.length; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        
+        return dp[n];
+    }
+}
