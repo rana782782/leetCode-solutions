8d3b3a55ef94da7969fc9d1840ee44370984852d
rana782782 <janardanbaitha3@gmail.com>
2025-06-04 12:26:14 +0000

    LeetCode: Nim Game (nim-game) — solved on 2025-06-04T12:26:14Z

---
+++ nim-game/nim-game.java
@@ -0,0 +1,11 @@
+// LeetCode: Nim Game (nim-game)
+// Submission ID: 1653696173
+// Language: java
+// Timestamp (UTC): 2025-06-04T12:26:14Z
+
+class Solution {
+    public boolean canWinNim(int n) {
+       return n%4 != 0;
+        
+    }
+}
