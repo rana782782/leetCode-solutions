2834258aa66e02b9fe1bfeb1ac16a434a751141b
rana782782 <janardanbaitha3@gmail.com>
2025-06-28 12:10:28 +0000

    LeetCode: Pow(x, n) (powx-n) — solved on 2025-06-28T12:10:28Z

---
+++ powx-n/powx-n.java
@@ -0,0 +1,10 @@
+// LeetCode: Pow(x, n) (powx-n)
+// Submission ID: 1679268099
+// Language: java
+// Timestamp (UTC): 2025-06-28T12:10:28Z
+
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
