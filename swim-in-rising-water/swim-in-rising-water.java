9841dee653eb6ea061582620349340ddec3634a6
rana782782 <janardanbaitha3@gmail.com>
2025-07-23 19:18:05 +0000

    LeetCode: Swim in Rising Water (swim-in-rising-water) — solved on 2025-07-23T19:18:05Z

---
+++ swim-in-rising-water/swim-in-rising-water.java
@@ -0,0 +1,58 @@
+// LeetCode: Swim in Rising Water (swim-in-rising-water)
+// Submission ID: 1708940851
+// Language: java
+// Timestamp (UTC): 2025-07-23T19:18:05Z
+
+class Solution {
+   class Pair implements Comparable<Pair>{
+		int row;
+		 int col;
+		 int wt;
+		 
+		 Pair(int row, int col , int wt){
+			 this.row = row;
+			  this.col = col;
+			  this.wt = wt;
+		 }
+		 
+		 public int compareTo(Pair o) {
+			 return this.wt-o.wt;
+		 }
+	}
+
+	
+	 public int swimInWater(int[][] grid) {
+	        
+		 boolean visited[][] = new boolean[grid.length][grid[0].length];
+		 int n = grid.length;
+		 int m = grid[0].length;
+		 
+		 PriorityQueue<Pair> pq = new PriorityQueue<>();
+		 pq.add(new Pair(0,0,grid[0][0]));
+		 int dir[][] = {{-1,0},{1,0},{0,-1},{0,1}};
+		 
+		 int max = Integer.MIN_VALUE;
+		 
+		 while(pq.size()!=0) {
+			 Pair rem = pq.poll();
+			 max = Math.max(max,rem.wt);
+			 if(rem.row == n-1 && rem.col == m-1) {
+				 return max;
+			 }
+			 visited[rem.row][rem.col] = true;
+			 
+			 for(int i = 0; i<4; i++) {
+				 int nRow = rem.row+dir[i][0];
+				 int nCol = rem.col+dir[i][1];
+				 
+				 if(nRow>=0 && nCol>=0 && nRow<grid.length && nCol<grid[0].length && visited[nRow][nCol] == false) {
+					 pq.add(new Pair(nRow,nCol,grid[nRow][nCol]));
+					//  visited[nRow][nCol] = true;
+					 
+				 }
+			 }
+		 }
+		 
+		 return max;
+	    }
+}
